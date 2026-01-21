fb7dc0c56abed4495908f2522a17fdd726f2505d
unknown <lukas.strnad222@gmail.com>
2026-01-21 23:03:06 +0100

    added skill_set

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { AboveTheFold } from './above-the-fold/above-the-fold';
 import { AbouteMe } from './aboute-me/aboute-me';
+import { SkillSet } from './skill-set/skill-set';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, AboveTheFold, AbouteMe],
+  imports: [RouterOutlet, Header, AboveTheFold, AbouteMe, SkillSet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
