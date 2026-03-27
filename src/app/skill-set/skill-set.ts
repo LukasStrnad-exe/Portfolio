73a2bb2197c15ea8b345a8d74c223c840835da98
Lukas Strnad <lukas.strnad222@gmail.com>
2026-03-27 14:55:20 +0100

    added img skill_set

--- src/app/skill-set/skill-set.ts
+++ src/app/skill-set/skill-set.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './skill-set.scss'
 })
 export class SkillSet {
+  images = [
+    "HTML.png", "CSS.png", "JS.png", "MD.png", "TS.png", "A.png", "Firebase.png",
+    "Git.png", "Rest- api.png", "mindset.png"
+  ];
 
+  constructor() {
+    this.images.forEach(img => {
+      let container = document.getElementById("skills");
+    });
+
+
+  }
 }
